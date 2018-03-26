d9dcc455ea0c877bb896c0798363cc5033ff09e6
LindaNeniSundali <37372264+LindaNeniSundali@users.noreply.github.com>
2018-03-26 20:37:12 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+ import WeatherMap from './src/comp/WeatherMap.js';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <WeatherMap />
+    );
+  }
+}
